77f33c8266aa8f82d9148188c1c532ca7b146e1f
Ashraf941 <ashrafm7100@gmail.com>
2022-10-16 23:07:14 +0200

    Better Version

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,79 +1,100 @@
-class Element:
-    def __init__(self,data=None):
-        self.data=data
+class node:
+
+    def __init__(self,value=None):
+        self.value=value
         self.next=None
 
-
-class LinkedList:
-    def __init__(self):
-        self.head=Element()
+class linked_list:
     
-    def append(self,value):
-        new_element=Element(value)
+    def __init__(self):
+        self.head=node()
+    
+    def append(self , new_node):
         current=self.head
-        while current.next != None:
+        while current.next:
             current=current.next
-        current.next=new_element
+        current.next=node(new_node)
     
     def length(self):
         total=0
         current=self.head
-        while current.next != None:
+        while current.next:
             current=current.next
             total+=1
-        return total    
+        print(f'Length = {total}')
     
     def display(self):
         l=[]
         current=self.head
-        while current.next != None:
+        while current.next:
             current=current.next
-            l.append(current.data)
+            l.append(current.value)
         print(l)
-
-    def get(self,index):
-        current=self.head
-        currentindex=0
-        while True:
-            current=current.next
-            if currentindex == index:
-                return current.data
-            currentindex+=1
     
-    def insert(self,newdata,position):
+    def get(self,position):
+        current = self.head
+        index = 0
+        while current.next:
+            current = current.next
+            if index == position:
+                print(f"Node #{position+1} is {current.value}")
+            index+=1
+        return
+
+    
+    def remove(self,position):
         current=self.head
         index=0
         while current.next:
+            last_node=current
             current=current.next
-            temp=current.next
-            if index == position-1:
-                current.next = Element(newdata)
-                current.next.next = temp
+            if index==position:
+                last_node.next=current.next
             index+=1
+        return
 
-    def delete(self,index):
+    def insert(self,new_node,position):
         current=self.head
-        currentindex=0
-        while True:
-            lastelement=current
+        new_node=node(new_node)
+        index=0
+        while current.next:
+            last_node = current
+            current = current.next
+            if index == position:
+                last_node.next = new_node
+                new_node.next = current
+            index += 1 
+        return        
+    
+    def replace(self,old_node,new_node):
+        current = self.head
+        new_node=node(new_node)
+        while current.next:
+            last_node=current
             current=current.next
-            if currentindex == index:
-                lastelement.next=current.next
-                return
-            currentindex+=1
-ll=LinkedList()
-ll.display()
+            if current.value == old_node:
+                current.value = new_node.value
+        return            
+
+
+
+
+
+ll=linked_list()
+ll.append(1)
 ll.append(2)
 ll.append(3)
 ll.append(4)
-ll.append(5)
+ll.append(6)
+ll.length()
 ll.display()
-ll.insert(3,3)
+ll.remove(2)
 ll.display()
-
-    
-
-
-
+ll.insert(3,2)
+ll.display()
+ll.insert(0,0)
+ll.display()
+ll.replace(6,5)
+ll.display()
 
 
